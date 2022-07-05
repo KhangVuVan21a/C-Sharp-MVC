415a919aba6b15a8cf1983527eeca317d6fd3ba2
KhangVuVan21a <khang21a@gmail.com>
2022-07-05 17:25:37 +0700

    fisrt commit

---
+++ test1/Controllers/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using test1.Models;
+
+namespace test1.Controllers
+{
+    public class StudentController : Controller
+    {
+        static IList<Student> studentList = new List<Student>{
+                new Student() { StudentId = 1, StudentName = "John", Age = 18 } ,
+                new Student() { StudentId = 2, StudentName = "Steve",  Age = 21 } ,
+                new Student() { StudentId = 3, StudentName = "Bill",  Age = 25 } ,
+                new Student() { StudentId = 4, StudentName = "Ram" , Age = 20 } ,
+                new Student() { StudentId = 5, StudentName = "Ron" , Age = 31 } ,
+                new Student() { StudentId = 4, StudentName = "Chris" , Age = 17 } ,
+                new Student() { StudentId = 4, StudentName = "Rob" , Age = 19 }
+            };
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View(studentList.OrderBy(i=>i.StudentId).ToList());
+        }
+        public ActionResult Edit(int Id)
+        {
+            var std = studentList.Where(s => s.StudentId == Id).FirstOrDefault();
+            return View(std);
+        }
+        [HttpPost]
+        public ActionResult Edit(Student std)
+        {
+            var student = studentList.Where(s => s.StudentId == std.StudentId).FirstOrDefault();
+            studentList.Remove(student);
+            studentList.Add(std);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ test1/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace test1.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        [Display(Name = "Name")]
+        public String StudentName { get; set; }
+        public int Age { get; set; }
+
+    }
+}
